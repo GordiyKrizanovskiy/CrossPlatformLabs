27f0faa7ecd1232a12751fdd2ee262cc1666ec86
gordi <gordiy.krizanovskiy@knu.ua>
2024-10-02 17:57:37 +0300

    create project + lab2

---
+++ lab2/lab2/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Program
+{
+    public static int MinimumOperations(int N)
+    {
+        int[] dp = new int[N + 1];
+        dp[1] = 0;
+        for (int i = 2; i <= N; i++)
+        {
+            dp[i] = dp[i - 1] + 1;
+            if (i % 2 == 0)
+            {
+                dp[i] = Math.Min(dp[i], dp[i / 2] + 1);
+            }
+            if (i % 3 == 0)
+            {
+                dp[i] = Math.Min(dp[i], dp[i / 3] + 1);
+            }
+        }
+        return dp[N];
+    }
+
+    public static void Main(string[] args)
+    {
+        int N = int.Parse(File.ReadAllText("INPUT.TXT"));
+        int result = MinimumOperations(N);
+        File.WriteAllText("OUTPUT.TXT", result.ToString());
+    }
+}
