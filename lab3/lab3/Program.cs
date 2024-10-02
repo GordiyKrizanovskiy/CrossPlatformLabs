d81c7624e30339b6da50fa3f91d7524aca3d7e73
gordi <gordiy.krizanovskiy@knu.ua>
2024-10-02 20:00:18 +0300

    code lab3 program

--- lab3/lab3/Program.cs
+++ lab3/lab3/Program.cs
@@ -1,2 +1,113 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Program
+{
+    public static void Main()
+    {
+        string[] input = File.ReadAllLines("INPUT.TXT");
+        int N = int.Parse(input[0]);
+        double[][] cities = new double[N][];
+        
+        for (int i = 1; i <= N; i++)
+        {
+            string[] coordinates = input[i].Split();
+            cities[i - 1] = new double[] { double.Parse(coordinates[0]), double.Parse(coordinates[1]) };
+        }
+
+        double minRadius = FindMinimumRadius(cities, N);
+
+        File.WriteAllText("OUTPUT.TXT", minRadius.ToString("F2"));
+    }
+
+    public static double FindMinimumRadius(double[][] cities, int N)
+    {
+        List<Tuple<int, int, double>> edges = new List<Tuple<int, int, double>>();
+        
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = i + 1; j < N; j++)
+            {
+                double distance = Math.Sqrt(Math.Pow(cities[i][0] - cities[j][0], 2) + Math.Pow(cities[i][1] - cities[j][1], 2));
+                edges.Add(new Tuple<int, int, double>(i, j, distance));
+            }
+        }
+        
+        edges.Sort((x, y) => x.Item3.CompareTo(y.Item3));
+        
+        UnionFind uf = new UnionFind(N);
+        double maxEdgeInMST = 0.0;
+        
+        foreach (var edge in edges)
+        {
+            if (!uf.Connected(edge.Item1, edge.Item2))
+            {
+                uf.Union(edge.Item1, edge.Item2);
+                maxEdgeInMST = edge.Item3;
+                
+                if (uf.Count == 1)
+                {
+                    break;
+                }
+            }
+        }
+        
+        return maxEdgeInMST;
+    }
+}
+
+public class UnionFind
+{
+    private int[] parent;
+    private int[] size;
+    public int Count { get; private set; }
+
+    public UnionFind(int n)
+    {
+        parent = new int[n];
+        size = new int[n];
+        Count = n;
+
+        for (int i = 0; i < n; i++)
+        {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    public int Find(int p)
+    {
+        if (p != parent[p])
+        {
+            parent[p] = Find(parent[p]);
+        }
+        return parent[p];
+    }
+
+    public void Union(int p, int q)
+    {
+        int rootP = Find(p);
+        int rootQ = Find(q);
+
+        if (rootP == rootQ) return;
+
+        if (size[rootP] > size[rootQ])
+        {
+            parent[rootQ] = rootP;
+            size[rootP] += size[rootQ];
+        }
+        else
+        {
+            parent[rootP] = rootQ;
+            size[rootQ] += size[rootP];
+        }
+
+        Count--;
+    }
+
+    public bool Connected(int p, int q)
+    {
+        return Find(p) == Find(q);
+    }
+}
