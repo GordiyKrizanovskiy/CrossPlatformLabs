c19ff97504d3cac66d3f3f6046ef5abeae18255d
gordi <gordiy.krizanovskiy@knu.ua>
2024-10-10 11:34:37 +0300

    .

--- lab3/add_package.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-REM Підключення NuGet пакету до консольного проекту
-dotnet add ./lab3/lab3.csproj package GK --source ./GK/bin/Release/
--- lab3/pack_and_publish.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Упаковка бібліотеки
-dotnet pack ./GK/GK.csproj -c Release
-
-REM Публікація в локальний NuGet репозиторій
-dotnet nuget add source ./GK/bin/Release/ -n LocalNugetRepo
